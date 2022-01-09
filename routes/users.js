67c817bf18e1f69bb9d56f1d53d5053eae999516
robert <robgladue@hotmail.com>
2022-01-09 19:42:44 +0000

    further work on client-side pages

--- routes/users.js
+++ routes/users.js
@@ -8,18 +8,26 @@
 const express = require('express');
 const router  = express.Router();
 
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    .then(data => {
+      const users = data.rows;
+      res.json({ users });
+    })
+    .catch(err => {
+      res
+      .status(500)
+      .json({ error: err.message });
+    });
+  });
+  router.get("/login", (req, res) => {
+  
+    res.render("login");
   });
   return router;
 };
+
+
+
